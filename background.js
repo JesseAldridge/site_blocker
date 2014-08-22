b8118dabcc060fd8c8a3e6099369e5d6dc3df835
Jesse Aldridge <jessealdridge@gmail.com>
2014-08-22 11:06:00 -0700

    initial

---
+++ background.js
@@ -0,0 +1,46 @@
+console.log('background.js')
+
+// Block sites for a while after clicking the block button.
+
+var num_minutes = 180
+
+var site_names = ['reddit', 'news.ycombinator', 'news.google']
+var click_time = null
+
+chrome.tabs.onUpdated.addListener(function(tabId) {
+  chrome.tabs.get(tabId, function(tab) {
+    console.log('tab:', tab)
+    if(!click_time || Date.now() - click_time > num_minutes * 60 * 1000)
+      return
+    for(var i = 0; i < site_names.length; i++) {
+      var name = site_names[i]
+      if(tab.url.indexOf(name) != -1) {
+        console.log('blocking')
+        chrome.tabs.executeScript(tabId, {
+          code: 'document.body.innerHTML = "blocked"'
+        });
+      }
+    }
+  })
+})
+
+chrome.browserAction.onClicked.addListener(function() {
+  click_time = new Date()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
