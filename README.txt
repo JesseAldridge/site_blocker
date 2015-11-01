a04870c7c19c8126a8bcd3af97f5144fff89e47f
Jesse Aldridge <JesseAldridge@gmail.com>
2015-10-31 20:17:57 -0700

    update readme

--- README.txt
+++ README.txt
@@ -1,6 +1,10 @@
+This is a Chrome extension that allows you to temporarily block certain sites while you're trying
+to work.
+
 To install:
 
-Clone the repo, click Settings -> Extensions
+Clone the repo
+In Chrome, click Settings -> Extensions
 Check "Developer Mode" box
 Click "Load Unpacked Extension"
 Select the directory of the cloned repo
--- background.js
+++ background.js
@@ -1,57 +1,64 @@
-console.log('background.js')
+console.log('background.js');
 
 // Set default black and whitelist values.
 
-chrome.storage.sync.get({
-  blacklist: [
-    'reddit', 'news.ycombinator', 'news.google',
-    'twitter.com/$', 'facebook', 'youtube', 'twitch'],
-  whitelist: ['cardbrew']
-}, function(items) {});
+// chrome.storage.sync.get({
+//   blacklist: [
+//     'reddit', 'news.ycombinator', 'news.google',
+//     'twitter.com/$', 'facebook', 'youtube', 'twitch', 'tumblr'],
+//   whitelist: ['cardbrew']
+// }, function(items) {});
 
-var num_minutes = 300
-var click_time = null
+var blacklist = [
+    'reddit', 'news.ycombinator', 'news.google',
+    'twitter.com/$', 'facebook', 'youtube', 'twitch', 'tumblr'];
+var whitelist = ['cardbrew'];
+
+
+
+var num_minutes = 300;
+var click_time = null;
 
 chrome.tabs.onUpdated.addListener(function(tabId) {
 
   // If button has been clicked and time hasn't expired, check if the tab is allowed.
 
   chrome.tabs.get(tabId, function(tab) {
-    console.log('tab:', tab)
+    console.log('tab:', tab);
     if(!click_time || Date.now() - click_time > num_minutes * 60 * 1000)
-      return
-    block_tab(tab)
-  })
-})
+      return;
+    block_tab(tab);
+  });
+});
 
 function block_tab(tab) {
 
   // Load black and whitelist.
 
-  var blacklist = null, whitelist = null
-  chrome.storage.sync.get(function(items) {
-    blacklist = items.blacklist
-    whitelist = items.whitelist
+  // var blacklist = null, whitelist = null
+  // chrome.storage.sync.get(function(items) {
+  //   blacklist = items.blacklist
+  //   whitelist = items.whitelist
 
     // Block the tab if a blacklist regex matches, unless a whitelist regex matches.
 
+    console.log('block_tab:', tab);
     for(var i = 0; i < whitelist.length; i++)
       if(tab.url.toLowerCase().indexOf(whitelist[i].toLowerCase()) != -1)
-        return
-    for(var i = 0; i < blacklist.length; i++) {
-      var name = blacklist[i]
-      var regex = 'https?:\/\/(www\.)?' + name
-      console.log('tab.url:', tab.url, 'regex:', regex)
+        return;
+    blacklist.forEach(function(name) {
+      var regex = 'https?:\/\/(www\.)?' + name;
+      console.log('tab.url:', tab.url, 'regex:', regex);
       if(tab.url.match(regex)) {
-        console.log('match, blocking')
+        console.log('match, blocking');
         chrome.tabs.executeScript(tab.id, {
           code: 'document.body.innerHTML = "blocked"'
         });
       }
-    }
-  })
+    });
+  // })
 }
 
 chrome.browserAction.onClicked.addListener(function() {
-  click_time = new Date()
-})
+  click_time = new Date();
+});
